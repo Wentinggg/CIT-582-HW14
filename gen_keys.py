860ae2080d678c80c8751932eb7bc83b443baa50
Wenting Zhao <88762053+Wentinggg@users.noreply.github.com>
2021-12-14 20:25:35 -0800

    Add files via upload

---
+++ gen_keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from algosdk import mnemonic
+from algosdk import account
+from web3 import Web3
+
+if __name__ == '__main__':
+    w3=Web3()
+    w3.eth.account.enable_unaudited_hdwallet_features()
+    acct, mnemonic_secret = w3.eth.account.create_with_mnemonic()
+    acct = w3.eth.account.from_mnemonic(mnemonic_secret)
+    eth_pk, eth_sk = acct._address, acct._private_key
+
+    print("eth secret key:", eth_sk)
+    print("eth public key", eth_pk)
+
