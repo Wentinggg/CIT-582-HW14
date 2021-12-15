c27f295795a36e4e2ca0b62c7f9fea31fc7b5a45
Wenting Zhao <88762053+Wentinggg@users.noreply.github.com>
2021-12-14 20:11:55 -0800

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -5,7 +5,7 @@ from algosdk import account
 from web3 import Web3
 
 if __name__ == '__main__':
-    w3=Web3()
+    w3 = Web3()
     w3.eth.account.enable_unaudited_hdwallet_features()
     acct, mnemonic_secret = w3.eth.account.create_with_mnemonic()
     acct = w3.eth.account.from_mnemonic(mnemonic_secret)
